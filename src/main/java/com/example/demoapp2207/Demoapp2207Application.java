7f63dcbca40d334d61dfdfc61af8ff92706c4af2
yaohao <123456>
2022-10-13 19:25:35 +0800

    Initial commit

---
+++ src/main/java/com/example/demoapp2207/Demoapp2207Application.java
@@ -0,0 +1,13 @@
+package com.example.demoapp2207;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoapp2207Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demoapp2207Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoapp2207/Demoapp2207ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoapp2207;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoapp2207ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
